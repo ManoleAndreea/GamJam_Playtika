fec7db14f605043689408c3b6a19dbe553fca57d
ManoleAndreea <manoleandreea2003@icloud.com>
2025-04-04 18:51:06 +0300

    Scene transition - MainMenu<->Scene1 without graphics

---
+++ Assets/SceneTransition.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneTransition:MonoBehaviour
+{
+    public Image fadePanel;
+    public float fadeDuration=1f;
+
+    public void StartGameWithFade(string sceneName)
+    {
+        StartCoroutine(FadeAndLoadScene(sceneName));
+    }
+
+    IEnumerator FadeAndLoadScene(string sceneName)
+    {
+        float t=0;
+        Color c=fadePanel.color;
+        while (t<fadeDuration)
+        {
+            t+=Time.deltaTime;
+            c.a=Mathf.Lerp(0, 1, t/fadeDuration);
+            fadePanel.color=c;
+            yield return null;
+        }
+
+    
+        SceneManager.LoadScene(sceneName);
+    }
+}
